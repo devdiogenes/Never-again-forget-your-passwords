655ba9b20657db177fb00f4030e98e5835d349c4
diogenes-souza-dev <diogenes.cruz2001@gmail.com>
2022-07-29 16:10:17 -0300

    Configured Passwords.json to save the master pass

--- index.py
+++ index.py
@@ -1,24 +1,68 @@
 import pyperclip
 import cryptocode
 import getpass
+import json
 
-class NeverAgainForgetYourPasswords:
-    def __init__(self): 
-        self.__master_pw = self.__ask_master_pw()
-        self.__pw = ""
-        self.__encrypted_pw = ""
+class DatabaseNeverAgainForgetYourPasswords:
+    def __init__(self):
+        self.__data = self.__open_data()
+        self.__testing_pw = "diogenes-souza-dev"
 
-    def __ask_master_pw(self):
+    def __open_data(self):
+        try: 
+            json_file = open("Passwords.json", "r")
+            json_data = json.loads(json_file.read())
+            return json_data
+        except: 
+            self.__data = {}
+            self._write_data()
+            self.__open_data()
+    
+    def _check_master_pw_setted(self):
+        try:
+            self.__data["encrypted_master_pw"]
+        except: 
+            self.__set_new_master_pw()
+
+    def _login(self, master_pw):
+        decrypted_master_pw = cryptocode.decrypt(self.__data["encrypted_master_pw"], master_pw)
+        if decrypted_master_pw == self.__testing_pw:
+            return master_pw
+        else:
+            return False
+
+    def __set_new_master_pw(self):
         master_pw = False
-        while master_pw == False:
-            try1 = getpass.getpass("\nType the master password... \n>")
-            try2 = getpass.getpass("Type the master password again... \n>")
+        while master_pw == False: 
+            try1 = getpass.getpass("\nCreate a master password. It will be used to decrypt the passwords you will save here...\n")
+            try2 = getpass.getpass("Repeat the master password you just created...\n")
             if try1 == try2:
                 master_pw = try1
-            else: 
-                master_pw = False
-                print("\nTry again... \n")
-        return master_pw    
+                input("Master password set successfully")
+            else:
+                input("The passwords are different. Try again...")
+
+        encrypted_master_pw = cryptocode.encrypt(self.__testing_pw, master_pw)
+        self.__data["encrypted_master_pw"] = encrypted_master_pw
+        self._write_data()
+        return self.__data
+
+    def _write_data(self):
+        open("Passwords.json", "w").write(json.dumps(self.__data))
+
+
+class NeverAgainForgetYourPasswords(DatabaseNeverAgainForgetYourPasswords):
+    def __init__(self): 
+        self.__master_pw = False
+        self.__pw = ""
+        self.__encrypted_pw = ""
+        self.__database = DatabaseNeverAgainForgetYourPasswords()
+        self.__database.__init__()
+
+    def __ask_master_pw(self):
+        self.__database._check_master_pw_setted()
+        while self.__master_pw == False:
+            self.__master_pw = self.__database._login(getpass.getpass("\nType the master password... \n>"))
 
     def __encrypt(self): 
         self.__pw = input("\nType the password you want to encrypt... \n>")
@@ -54,7 +98,9 @@ class NeverAgainForgetYourPasswords:
             self.__encrypt_or_decrypt()
 
     def run(self):
+        self.__ask_master_pw()
         self.__encrypt_or_decrypt()
 
 
 NeverAgainForgetYourPasswords().run()
+#NeverAgainForgetYourPasswords()
